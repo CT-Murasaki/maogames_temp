4db0d89e6faabb7fd0763dd8c9b47ee9e50f64a7
RS☆murasaki <rintam65@gmail.com>
2024-04-26 18:23:54 +0900

    Debug2

--- script/main.js
+++ script/main.js
@@ -42,12 +42,13 @@ function makescene(param) {
         let gametime = 0; // ゲーム時間測定
         let warmup = 0; //ゲーム開始猶予
         let startstate = false;
-        let finishstate = true;
+        let finishstate = false;
         let score = 0;
+        let combo = 0;
+        let hitpoint = 10;
         
         let bodyNo = 1;
         let bodytotal = 6;
-        let findkind = 2;
 
         // レイヤーの生成
         let startlayer = new g.E({ scene: scene, parent: scene });  //スタート画面
@@ -56,26 +57,31 @@ function makescene(param) {
         let buttonlayer = new g.E({ scene: scene, parent: scene }); //ターゲットレイヤー(映らない)
         
         // フォントの生成
-        let glyph = JSON.parse(scene.assets["font_round_glyphs"].data);
-        let font = new g.BitmapFont({src: scene.assets["font_round"],
-            map:glyph,defaultGlyphWidth: 96,defaultGlyphHeight: 96,});
+        let glyph1 = JSON.parse(scene.assets["font_round_glyphs"].data);
+        let font1 = new g.BitmapFont({src: scene.assets["font_round"],
+            map:glyph1,defaultGlyphWidth: 96, defaultGlyphHeight: 96});
+        let font = new g.DynamicFont({
+            game: g.game,
+            fontFamily: "sans-serif",
+            size: 45
+          });
 
         //タイトル画面
         let startimage = new g.FrameSprite({scene: scene, src: scene.assets["title"], parent: startlayer,
-            x: g.game.width/2, y: g.game.height/2,anchorX: 0.5, anchorY: 0.5, opacity: 1, touchable: true});
+            x: g.game.width/2, y: g.game.height/2, anchorX: 0.5, anchorY: 0.5, opacity: 1, touchable: true});
         //クリックするとスタートするようにする
         startimage.onPointDown.add(function () {if (warmup < 10){
             startstate = true
         }});
 
         // スコア表示用のラベル
-        let scoreLabel = new g.Label({parent: backlayer, scene: scene,text: "SCORE: 0",
-            font: font,fontSize: font.size / 2,textColor: "black",opacity: 0});
+        let scoreLabel = new g.Label({parent: backlayer, scene: scene, text: "SCORE: 0",
+            font: font1, fontSize: font1.size / 2, textColor: "dimgray", opacity: 0});
         scene.append(scoreLabel);
 
         // 残り時間表示用ラベル
-        let timeLabel = new g.Label({parent: backlayer,scene: scene,text: "TIME: " + 60,font: font,fontSize: 
-            font.size / 2,textColor: "black",x: 0.65 * g.game.width,opacity: 0});
+        let timeLabel = new g.Label({parent: backlayer,scene: scene,text: "TIME: " + 60, 
+            font: font1, fontSize: font1.size / 2, textColor: "dimgray", x: 0.65 * g.game.width,opacity: 0});
         scene.append(timeLabel);
 
         //背景画像
@@ -87,108 +93,101 @@ function makescene(param) {
         let backbody = new g.FrameSprite({scene: scene, src: scene.assets["body" + bodyNo], parent: bodylayer,
             x: g.game.width/2, y: g.game.height/2,anchorX: 0.5, anchorY: 0.5, opacity: 0, touchable: false});
         //ターゲットラベル
-        let find = new g.Sprite({scene: scene, src: scene.assets["find"], parent: buttonlayer,
-            x: g.game.width/2, y: g.game.height/2, opacity: 0, touchable: false});
+        let find = new g.FilledRect({scene: scene, parent: buttonlayer,cssColor: "red",
+            width: 32, height: 32, x: g.game.width/2, y: g.game.height/2, opacity: 0, touchable: false});
+        scene.append(find);
        
         //体を触ると減点
         backbody.onPointDown.add(function(){
             score -= 50; 
+            combo = 0;
             scene.assets["se_hazure"].play().changeVolume(0.6);
         });
 
         //ターゲットを触るとスコアアップ
         find.onPointDown.add(function(){
-            score += 50; 
+            score += 50;
+            combo += 1;
+            hitpoint -= 1;
+            if (hitpoint == 0){
+                hitpoint = 10;
+                bodyNo += 1;
+                backbody.src = scene.assets["body" + bodyNo];
+            }
             scene.assets["se_seikai"].play().changeVolume(0.6);
         });
 
-        let mainObj = [startimage,scoreLabel,timeLabel,backbody,find];
+        // ターゲット名称ラベルヘッダー
+        let TargetHeder = new g.Label({parent: backlayer,scene: scene,text: "Target", font: font1,
+            fontSize: font1.size / 2, textColor: "darkorange", y: 0.60 * g.game.height, opacity: 0});
+        scene.append(TargetHeder);
 
-        let targetLabel = [];
-        let nextLabel = [];
+        // ターゲット名称ラベル
+        let TargetLabel = new g.Label({parent: backlayer,scene: scene,text: "インナー", font: font,
+            fontSize: font.size , textColor: "black", y: 0.70 * g.game.height, opacity: 0});
+        scene.append(TargetLabel);
+    
+        // 次ターゲット名称ラベルヘッダー
+        let NextHerder = new g.Label({parent: backlayer,scene: scene,text: "Next", font: font1,
+            fontSize: font1.size / 2, textColor: "darkorange", y: 0.40 * g.game.height, opacity: 0});
+        scene.append(NextHerder);
+
+        // 次ターゲット名称ラベル
+        let NextLabel = new g.Label({parent: backlayer,scene: scene,text: "b", font: font,
+            fontSize: font.size , textColor: "black", y: 0.50 * g.game.height, opacity: 0});
+        scene.append(NextLabel);
+
+        let mainObj = [background,scoreLabel,timeLabel,backbody,
+                        TargetHeder,TargetLabel,NextHerder,NextLabel];
 
         // ■■■■■■■■■■■■　 メイン描画　　■■■■■■■■■■■■
         scene.onUpdate.add(function () {//時間経過
-            warmup += 1 / g.game.fps;
-            if (startstate == true) { 
-                startimage.opacity = 0;
+            if (startstate == true && finishstate == false) { 
                 gametime += 1 / g.game.fps; 
 
-                if (gametime <= 60){
-                    scoreLabel.opacity = 1;
+                startimage.opacity = 0;
+                startimage.touchable = false;
+                mainObj.forEach(function(Obj){Obj.opacity = 1;});
+                find.opacity = 1;
+                backbody.touchable = true;
+                find.touchable = true;
+
+                if (gametime <= 60 && bodyNo != bodytotal){
                     scoreLabel.text = "SCORE: " + score;
-                    timeLabel.opacity = 1;
                     timeLabel.text = "TIME: " + Math.ceil(60 -gametime);
-                    background.opacity = 1;
                 }
                 else{
-
+                    finishstate = true
                 };
             } 
+            else if (finishstate == true){
+                startstate = false
+                backbody.touchable = false;
+                find.touchable = false;
+                startimage.opacity = 0;
+                find.opacity = 0;
+                startimage.touchable = false;
+                mainObj.forEach(function(Obj){Obj.opacity = 0;});
+            }
             else
             {
-                if (warmup < 20){
+                warmup += 1 / g.game.fps;
+                if (warmup < 15){
                     startimage.opacity = 1;
                 }
                 else{
-                    startimage.opacity = 0; startimage.touchable = false;
+                    finishstate = true
                 };
             };
-            mainObj.forEach(function(Obj){
-                Obj.invalidate();
-            });
+
+
+            startimage.invalidate();
+            find.modified()
+            mainObj.forEach(function(Obj){Obj.invalidate();});
         });
-        // ここまでゲーム内容を記述します
-
-    // g.game.pushScene(scene);
+    });
+    // ここまでゲーム内容を記述します
     return scene;
-    })
-}
+};
 
-
-function backbodyAdd(scene,bodylayer,bodyNo){
-    let backbody = new g.FrameSprite({ //体
-        scene: scene, src: scene.assets["body" + bodyNo], parent: bodylayer,
-        x: g.game.width/2, y: g.game.height/2,
-        anchorX: 0.5, anchorY: 0.5, opacity: 0
-    });
-    backbody.onPointDown.add(function(){
-        if (backbody.opacity = 1){
-            g.game.vars.gameState.score -= 50;
-            scene.assets["se_hazure"].play().changeVolume(0.6);
-        }
-    })
-    return backbody;
-}
-
-
-function targetAdd(scene,backlayer,font,str,cnt,next){
-    let text;
-    if (cnt == 1){
-        text = str;
-    }
-    else{
-        text = str + "x" + cnt;
-    }
-
-    let y;
-    if (next == true){
-        y = 0.3;
-    }
-    else{
-        y = 0.2;
-    }
-    let targetLabel = new g.Label({
-        parent: backlayer,
-        scene: scene,
-        text: text,
-        font: font,
-        fontSize: font.size / 2,
-        textColor: "black",
-        x: 0.65 * g.game.width,
-        y: y * g.game.height,
-        opacity: 0
-    });
-    return targetLabel;
-}
 exports.main = main;

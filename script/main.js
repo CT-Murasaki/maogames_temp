00549538045755065429ee8038fccbfdf9ab2732
RS☆murasaki <rintam65@gmail.com>
2024-04-28 20:55:45 +0900

    Debug2

--- script/main.js
+++ script/main.js
@@ -6,6 +6,8 @@ function main(param) {
 
 function makescene(param) {
 
+    const debugmode = true;
+
     let scene = new g.Scene({
         game: g.game,
         // このシーンで利用するアセットのIDを列挙し、シーンに通知します
@@ -13,7 +15,6 @@ function makescene(param) {
             "title",
             "font_round",
             "font_round_glyphs",
-            "find",
             "haikei",
             "body1",
             "body2",
@@ -34,7 +35,6 @@ function makescene(param) {
 
     // 市場コンテンツのランキングモードでは、g.game.vars.gameState.score の値をスコアとして扱います
     g.game.vars.gameState = { score: 0 };
-
     scene.onLoad.add(function () {
         // ここからゲーム内容を記述します
         //■■■■■■■■■■■■　 変数　レイヤー　フォント 　■■■■■■■■■■■■
@@ -46,6 +46,8 @@ function makescene(param) {
         let score = 0;
         let combo = 0;
         let hitpoint = 10;
+        let partsId = 0;
+        let Nextparts = 0;
         
         let bodyNo = 1;
         let bodytotal = 6;
@@ -54,7 +56,7 @@ function makescene(param) {
         let startlayer = new g.E({ scene: scene, parent: scene });  //スタート画面
         let backlayer = new g.E({ scene: scene, parent: scene });   //背景
         let bodylayer = new g.E({ scene: scene, parent: scene });   //体(押すとはずれ)
-        let buttonlayer = new g.E({ scene: scene, parent: scene }); //ターゲットレイヤー(映らない)
+        let buttonlayer = new g.E({ scene: scene, parent: scene}); //ターゲットレイヤー(映らない)
         
         // フォントの生成
         let glyph1 = JSON.parse(scene.assets["font_round_glyphs"].data);
@@ -92,9 +94,24 @@ function makescene(param) {
         //体
         let backbody = new g.FrameSprite({scene: scene, src: scene.assets["body" + bodyNo], parent: bodylayer,
             x: g.game.width/2, y: g.game.height/2,anchorX: 0.5, anchorY: 0.5, opacity: 0, touchable: false});
+        
         //ターゲットラベル
-        let find = new g.FilledRect({scene: scene, parent: buttonlayer,cssColor: "red",
-            width: 32, height: 32, x: g.game.width/2, y: g.game.height/2, opacity: 0, touchable: false});
+        let t_list = [];
+        let t_str = ["","シャツ(左)","シャツ(右)","シャツ(むね)","短パン","ひだりあし","みぎあし","しっぽ"]
+        partsId = getrandom(1, 7, -1);
+        Nextparts = getrandom(1, 7, partsId);
+        let t_int = partsId;
+        let t_hp = getrandom(1, 4, -1);
+        let Nexthp = getrandom(1, 4, -1);
+        t_list[1] = [392,247,121,148] //シャツ(左)
+        t_list[2] = [624,247,112,150] //シャツ(右)
+        t_list[3] = [495,236,144,158] //シャツ(むね)
+        t_list[4] = [469,392,199,163] //短パン
+        t_list[5] = [476,554,93,164] //ひだりあし
+        t_list[6] = [576,554,93,164] //みぎあし
+        t_list[7] = [613,369,158,146] //しっぽ
+        let find = new g.FilledRect({scene: scene,x:t_list[t_int][0], y:t_list[t_int][1], width: t_list[t_int][2], height:t_list[t_int][3], 
+            cssColor: "black", parent: buttonlayer, opacity: 0});
         scene.append(find);
        
         //体を触ると減点
@@ -109,32 +126,44 @@ function makescene(param) {
             score += 50;
             combo += 1;
             hitpoint -= 1;
+            t_hp -= 1;
+
             if (hitpoint == 0){
                 hitpoint = 10;
                 bodyNo += 1;
                 backbody.src = scene.assets["body" + bodyNo];
+            }
+            else if (t_hp == 0){
+                t_int = Nextparts
+                t_hp = Nexthp;
+                find.x = t_list[t_int][0]
+                find.y = t_list[t_int][1]
+                find.width = t_list[t_int][2]
+                find.height = t_list[t_int][3]
+                Nextparts = getrandom(1, 7, t_int);
+                Nexthp = getrandom(1, 4, -1);
             }
             scene.assets["se_seikai"].play().changeVolume(0.6);
         });
 
         // ターゲット名称ラベルヘッダー
         let TargetHeder = new g.Label({parent: backlayer,scene: scene,text: "Target", font: font1,
-            fontSize: font1.size / 2, textColor: "darkorange", y: 0.60 * g.game.height, opacity: 0});
+            fontSize: font1.size / 2, textColor: "darkorange", y: 0.40 * g.game.height, opacity: 0});
         scene.append(TargetHeder);
 
         // ターゲット名称ラベル
-        let TargetLabel = new g.Label({parent: backlayer,scene: scene,text: "インナー", font: font,
-            fontSize: font.size , textColor: "black", y: 0.70 * g.game.height, opacity: 0});
+        let TargetLabel = new g.Label({parent: backlayer,scene: scene,text: getbodyName(t_str[t_int],t_hp), font: font,
+            fontSize: font.size , textColor: "black", y: 0.50 * g.game.height, opacity: 0});
         scene.append(TargetLabel);
     
         // 次ターゲット名称ラベルヘッダー
         let NextHerder = new g.Label({parent: backlayer,scene: scene,text: "Next", font: font1,
-            fontSize: font1.size / 2, textColor: "darkorange", y: 0.40 * g.game.height, opacity: 0});
+            fontSize: font1.size / 2, textColor: "darkorange", y: 0.60 * g.game.height, opacity: 0});
         scene.append(NextHerder);
 
         // 次ターゲット名称ラベル
-        let NextLabel = new g.Label({parent: backlayer,scene: scene,text: "b", font: font,
-            fontSize: font.size , textColor: "black", y: 0.50 * g.game.height, opacity: 0});
+        let NextLabel = new g.Label({parent: backlayer,scene: scene,text: getbodyName(t_str[Nextparts],Nexthp), font: font,
+            fontSize: font.size , textColor: "black", y: 0.70 * g.game.height, opacity: 0});
         scene.append(NextLabel);
 
         let mainObj = [background,scoreLabel,timeLabel,backbody,
@@ -148,9 +177,11 @@ function makescene(param) {
                 startimage.opacity = 0;
                 startimage.touchable = false;
                 mainObj.forEach(function(Obj){Obj.opacity = 1;});
-                find.opacity = 1;
+                if (debugmode == true){find.opacity = 0.5;}
                 backbody.touchable = true;
                 find.touchable = true;
+                TargetLabel.text = getbodyName(t_str[t_int],t_hp);
+                NextLabel.text = getbodyName(t_str[Nextparts],Nexthp);
 
                 if (gametime <= 60 && bodyNo != bodytotal){
                     scoreLabel.text = "SCORE: " + score;
@@ -180,9 +211,8 @@ function makescene(param) {
                 };
             };
 
-
             startimage.invalidate();
-            find.modified()
+            find.modified();
             mainObj.forEach(function(Obj){Obj.invalidate();});
         });
     });
@@ -190,4 +220,19 @@ function makescene(param) {
     return scene;
 };
 
+function getrandom(min,max,exc){
+    let int = g.game.random.get(min, max);
+    while(exc != -1 && exc == int){
+        int = g.game.random.get(min, max);
+    }
+    return int
+}
+
+function getbodyName(str,hp){
+    let bodyName = str;
+    if(hp != 1){
+        bodyName = str + "×" + String(hp);
+    }
+    return bodyName;
+}
 exports.main = main;
